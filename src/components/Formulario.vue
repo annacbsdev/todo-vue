b3734cdbb37d2e37003399a10604be3687a562f2
Anna Claudia <annaclaudiadev24@gmail.com>
2024-04-10 19:07:10 -0300

    projeto to-do-list completo

---
+++ src/components/Formulario.vue
@@ -0,0 +1,24 @@
+<template>
+    <form @submit.prevent="props.cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="props.tarefaNova" @change="props.editaTarefaNova" required
+                    type="text" placeholder="Digite a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-primary" type="submit">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="props.trocarFiltro" class="form-control">
+                    <option value="todas">Todas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+const props = defineProps(['cadastraTarefa', 'tarefaNova', 'trocarFiltro', 'editaTarefaNova'])
+</script>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas" :key="tarefa">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" :for="tarefa.titulo" class="ms-3">{{ tarefa.titulo }}</label>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+const props = defineProps(['tarefas'])
+</script>
+
+<style>
+.done{
+  text-decoration: line-through;
+}</style>
